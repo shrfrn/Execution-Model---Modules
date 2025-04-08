ea7da2b917a18fa63ddfbe55ad297e6d6503abaf
shrfrn <shrfrn@gmail.com>
2025-04-08 17:51:48 +0300

    Change demo setup

--- JS Modules/Hello Modules/js/hello-modules.js
+++ JS Modules/Hello Modules/js/hello-modules.js
@@ -1,12 +1,19 @@
 gCount = 0
 
+// TODO: Convert to module
+// TODO: Call a function from a service
+
 function onInit() {
-    strDate = Date()
-    console.log(strDate, 'Ready...')
+    randomNum = getRandomInt(0, 100)
+    console.log('Ready with random number -', randomNum)
 }
 
-function onGo() {
-    console.log('Going...', gCount++)
+function onIncrement() {
+    console.log('Incrementing...', gCount++)
 
-    // TODO: Call a function from a service
+}
+
+function onReset() {
+    gCount = 0
+    console.log('Reset complete', gCount)
 }
