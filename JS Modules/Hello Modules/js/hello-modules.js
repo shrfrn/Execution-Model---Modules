235aadc5f8c63d15e8455701ec1d44915136894e
shrfrn <shrfrn@gmail.com>
2025-11-18 11:01:18 +0200

    Refactor demo

--- JS Modules/Hello Modules/js/hello-modules.js
+++ JS Modules/Hello Modules/js/hello-modules.js
@@ -1,7 +1,8 @@
 gCount = 0
 
 // TODO: Convert to module
-// TODO: Call a function from a service
+// TODO: Call a function from a service (export function)
+// TODO: Create a score service and access it (export service object)
 
 function onInit() {
     randomNum = getRandomInt(0, 100)
